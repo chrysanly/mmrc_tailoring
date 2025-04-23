050471a0d9eaecce14ab6375d2693695569e877f
chrysanly <chrys.romao21@gmail.com>
2025-04-23 23:27:32 +0800

    in progress email content depend on order type

--- resources/views/mail/send-user-order-status-in-progress.blade.php
+++ resources/views/mail/send-user-order-status-in-progress.blade.php
@@ -1,10 +1,15 @@
 <x-mail::message>
-    **Dear Customer,**  
+    **Dear Customer,**
 
-    Your order is now in progress. Our tailoring team is already working on it, 
-    and we’ll update you once it’s ready. Estimated completion time of your ready-made order is 4-5 days 
+    @if ($order->order_type === 'Customized')
+        Your order is now in progress. Our tailoring team is already working on it,
+        and we’ll update you once it’s ready. Estimated completion time of your customized order is 6-7 days
+    @else
+        Your order is now in progress. Our tailoring team is already working on it,
+        and we’ll update you once it’s ready. Estimated completion time of your ready-made order is 4-5 days
+    @endif
 
-    Thank you for choosing MMRC Tailoring! We appreciate your trust in us. 
+    Thank you for choosing MMRC Tailoring! We appreciate your trust in us.
 
     Best regards,
     MMRC Tailoring ✂️
